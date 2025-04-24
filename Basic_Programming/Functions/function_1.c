f16ca54a3300ee6bd7f1c29d86000b0f1406def9
jaber20106 <azharulislamjaber20106@gmail.com>
2025-04-24 22:31:09 +0600

    new code added

---
+++ Basic_Programming/Functions/function_1.c
@@ -0,0 +1,17 @@
+//Return + Parameter
+
+int sum(int x,int y){
+    int sum = x + y;
+    return sum;
+}
+
+
+#include<stdio.h>
+
+int main(){
+    
+    int a = sum(10,20);
+    printf("%d",a);
+
+    return 0;
+}
---
+++ Basic_Programming/Functions/math_functions.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    
+    double x;
+    scanf("%lf",&x);
+    int ans = ceil(x);
+    printf("%d",ans);
+
+    return 0;
+}
+
+/*
+ceil()
+floor()
+round()
+sqrt()
+power()
+abs()
+*/
