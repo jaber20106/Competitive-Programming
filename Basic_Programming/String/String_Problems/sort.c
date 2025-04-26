1cf18b1634d5e70950bb09133053fe30d02e75ea
jaber20106 <azharulislamjaber20106@gmail.com>
2025-04-27 00:02:59 +0600

    code added

--- Basic_Programming/String/String_Problems/sort.c
+++ Basic_Programming/String/String_Problems/sort.c
@@ -8,6 +8,18 @@ int main() {
         char s[4];
         scanf("%s", s);
         
+        if(s[0] == 'a'){
+            printf("YES\n");
+        }
+        else if(s[1] == 'b'){
+            printf("YES\n");
+        }
+        else if(s[2] == 'c'){
+            printf("YES\n");
+        }
+        else{
+            printf("NO\n");
+        }
     }
 
     return 0;
