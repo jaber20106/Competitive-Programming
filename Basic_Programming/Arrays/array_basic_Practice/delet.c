2c2e637bd25b8f3b6d531d36188299cd9b144c67
jaber20106 <azharulislamjaber20106@gmail.com>
2025-05-05 23:52:59 +0600

    code

--- Basic_Programming/Arrays/array_basic_Practice/delet.c
+++ Basic_Programming/Arrays/array_basic_Practice/delet.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int arr[100] = {2, 4, 6, 8, 10}; // Initial array
-    int size = 5; // Current size of the array
-
-    int positionToDelete = 2; // Index of the element to delete
-
-    // Update the code to shift elements to fill the gap left by the deleted element
-
-    for(int i = positionToDelete; i < size - 1; i++){
-        arr[i] = arr[i + 1];
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d",&arr[i]);
     }
 
-    size--;
+    int pos;
+    scanf("%d",&pos);
 
-
-
-    // Update the size of the array
-
-
-    // Print the updated array
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
+    for(int i = pos; i < n - 1; i++){
+        arr[i] = arr[i + 1];
+    }
+    for(int i = 0; i < n-1; i++){
+        printf("%d ",arr[i]);
     }
 
     return 0;
