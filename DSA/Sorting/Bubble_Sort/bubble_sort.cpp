c57ec242c0eb2497da66f2f98b8d8de62c41f83d
jaber20106 <azharulislamjaber20106@gmail.com>
2025-06-27 10:27:16 +0600

    code change

--- DSA/Sorting/Bubble_Sort/bubble_sort.cpp
+++ DSA/Sorting/Bubble_Sort/bubble_sort.cpp
@@ -14,10 +14,15 @@ int main(){
 
     for(int i = 0; i < n-1; i++){
         //traverse
+        bool flag = true;
         for(int j = 0; j < n-1; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                flag = false;
             }
+        }
+        if(flag == true){
+            break;
         }
     }
     for(int i = 0; i < n; i++){
