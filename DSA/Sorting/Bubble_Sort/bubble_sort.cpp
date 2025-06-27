c743fc88e43d1732d7d9900a1d97d1934322c7bb
jaber20106 <azharulislamjaber20106@gmail.com>
2025-06-27 10:20:14 +0600

    change

--- DSA/Sorting/Bubble_Sort/bubble_sort.cpp
+++ DSA/Sorting/Bubble_Sort/bubble_sort.cpp
@@ -12,20 +12,17 @@ int main(){
         cin>>arr[i];
     }
 
-    // insertion sort
-    for(int i = 1; i < n; i++){
-        int j = i;
-        
-        while(j >= 1 && arr[j] < arr[j-1]){
-            swap(arr[j],arr[j-1]);
-            j--;
+    for(int i = 0; i < n-1; i++){
+        //traverse
+        for(int j = 0; j < n-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
         }
     }
-    
     for(int i = 0; i < n; i++){
         cout<<arr[i]<<" ";
     }
-    cout<<endl;
 
     return 0;
 }
